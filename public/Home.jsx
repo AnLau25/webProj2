2a5c4a7128918073e9a7b5085a75a4fa1e315e93
AnLau25 <avald024@uottawa.ca>
2024-06-30 13:00:35 -0400

    da

---
+++ public/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <section className='slideshow' id='home'>
+            <div className='Carrousel'>
+                <Carousel>
+                    <Carousel.Item>
+                        <img src='../public/JULES VERNE (c)Pop Films et 13 Prods.jpg' text="First slide" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src='../public/69309ab082550bebe755a7094c28e507 2.svg'text="Second slide" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src='../public/69309ab082550bebe755a7094c28e507 2.svg'text="Third slide" />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import GoldBtn from '../public/GoldBtn'
 import Navnav from '../public/Navnav'
+import Home from '../public/Home'
 
 function App() {
 
   return (
     <>
      <Navnav/>
+     <Home/>
     </>
   )
 }
