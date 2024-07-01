bd9470f628625b0e6514917a64172512193736cd
AnLau25 <avald024@uottawa.ca>
2024-07-01 01:06:25 -0400

    CorrectionIWillCry

--- public/Home.jsx
+++ public/Home.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import { Carousel, Row, Col, Container } from 'react-bootstrap';
 import './Home.css';
 
 const Home = () => {
     return (
         <section className='slideshow' id='home'>
-            <div className='Carrousel'>
+            <div className='carousel'>
                 <Carousel>
                     <Carousel.Item>
-                        <img src='../JULES VERNE (c)Pop Films et 13 Prods.jpg' text="First slide" />
+                        <img className='d-block w-100' src='../JULES VERNE (c)Pop Films et 13 Prods 1.svg' alt="First slide" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src='../69309ab082550bebe755a7094c28e507 2.svg'text="Second slide" />
+                        <img className='d-block w-100' src='../La-Edad-de-Oro-fotos-Kaloian-9.svg' alt="Second slide" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src='../69309ab082550bebe755a7094c28e507 2.svg'text="Third slide" />
+                        <img className='d-block w-100' src='../image_1.svg' alt="Third slide" />
                     </Carousel.Item>
                 </Carousel>
+            </div>
+            <div className='text-container'>
+                <h3>Welcome Readers!</h3>
+                <p>Come experience the ultimate reading experience in our cozy café, with flavorful snacks and a collection of classics that accommodates everyone’s taste. </p>
+                <div className='nav-links'>
+                    <a href="#about" className='nav-link'>About us</a>
+                    <a href="#contact" className='nav-link'>Events & Activities</a>
+                </div>
             </div>
         </section>
     );
