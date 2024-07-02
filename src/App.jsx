46d7e0dc1f28eb12e8dff40faab90f3859e9a25d
AnLau25 <avald024@uottawa.ca>
2024-07-02 01:52:59 -0400

    abtus

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import GoldBtn from '../public/GoldBtn'
-import Navnav from '../public/Navnav'
 import GoldSep from '../public/GoldSep'
+import Navnav from '../public/Navnav'
 import Home from '../public/Home'
+import AbtUs from '../public/AbtUs'
+
 
 function App() {
 
   return (
     <>
-      <Navnav />
-      <GoldSep/>
+      <Navnav/>
+      <AbtUs/>
     </>
   )
 }
