47fbe03f7074cf42e3ed381bd22d0ee1076f0629
AnLau25 <avald024@uottawa.ca>
2024-07-03 01:27:00 -0400

    x

--- public/AbtUs.jsx
+++ public/AbtUs.jsx
@@ -5,8 +5,6 @@ import GoldBtn from './GoldBtn';
 import { Row, Col, Container } from 'react-bootstrap';
 
 const AbtUs = () => {
-    const mapStyle = '[%7B%22elementType%22%3A%22geometry%22%2C%22stylers%22%3A%5B%7B%22color%22%3A%22%23212121%22%7D%5D%7D%2C%20...%20%5D]'; // This should be your URL encoded JSON.
-
     return (
         <div className='AbtUs'>
             <Container fluid>
@@ -37,7 +35,7 @@ const AbtUs = () => {
                     <Col xs={12} md={6} className='map-container'>
                         <iframe
                             className='map'
-                            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.145284844184!2d-5.98513802426236!3d37.41004077207962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd12695622594341%3A0x4e3db2dfd4d3d37!2zQy4gT3Rvw7FvLCAxLCA0MTAwOSBTZXZpbGxhLCBFc3Bhw7Fh!5e0!3m2!1ses!2sca!4v1719981251292!5m2!1ses!2sca=roadmap&style=${mapStyle}`}
+                            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.145284844184!2d-5.98513802426236!3d37.41004077207962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd12695622594341%3A0x4e3db2dfd4d3d37!2zQy4gT3Rvw7FvLCAxLCA0MTAwOSBTZXZpbGxhLCBFc3Bhw7Fh!5e0!3m2!1ses!2sca!4v1719981251292!5m2!1ses!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade`}
                             allowFullScreen>
                         </iframe>
                     </Col>
