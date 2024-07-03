274773b7fb0fd0c8361e888e72a139536714b341
AnLau25 <avald024@uottawa.ca>
2024-07-03 00:46:40 -0400

    s

--- public/AbtUs.jsx
+++ public/AbtUs.jsx
@@ -35,8 +35,8 @@ const AbtUs = () => {
                     <Col xs={12} md={6} className='map-container'>
                         <iframe
                             className='map'
-                            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20047.237136317344!2d-6.002739966721597!3d37.40999743042508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126956225ef4d1%3A0x3497b39c342f5615!2sEl%20Arepazo%20Sevilla!5e0!3m2!1ses!2sca!4v1719981068877!5m2!1ses!2sca`}
-                            allowFullScreen>
+                            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.145284844184!2d-5.98513802426236!3d37.41004077207962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd12695622594341%3A0x4e3db2dfd4d3d37!2zQy4gT3Rvw7FvLCAxLCA0MTAwOSBTZXZpbGxhLCBFc3Bhw7Fh!5e0!3m2!1ses!2sca!4v1719981251292!5m2!1ses!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade`}
+                            async defer allowFullScreen>
                         </iframe>
                     </Col>
                 </Row>
