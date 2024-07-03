9add1f7547eb3a8d269aafda82967410df9cbe0f
AnLau25 <avald024@uottawa.ca>
2024-07-03 00:32:57 -0400

    e

--- public/AbtUs.jsx
+++ public/AbtUs.jsx
@@ -32,8 +32,12 @@ const AbtUs = () => {
                             <GoldBtn href="#" prop='Check out the menu' />
                         </div>
                     </Col>
-                    <Col xs={12} md={6} className='map'>
-                        <h1>Location Map</h1>
+                    <Col xs={12} md={6} className='map-container'>
+                        <iframe
+                            className='map'
+                            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20047.237136317344!2d-6.002739966721597!3d37.40999743042508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126956225ef4d1%3A0x3497b39c342f5615!2sEl%20Arepazo%20Sevilla!5e0!3m2!1ses!2sca!4v1719981068877!5m2!1ses!2sca`}
+                            allowFullScreen>
+                        </iframe>
                     </Col>
                 </Row>
             </Container>
