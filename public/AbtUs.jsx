84cc3f9850f80f4b28ca74731a494f6413f5c897
AnLau25 <avald024@uottawa.ca>
2024-07-02 21:47:04 -0400

    dep

--- public/AbtUs.jsx
+++ public/AbtUs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './AbtUs.css';
 import GoldSep from './GoldSep';
 import GoldBtn from './GoldBtn';
@@ -7,36 +7,36 @@ import { Row, Col, Container } from 'react-bootstrap';
 const AbtUs = () => {
     return (
         <div className='AbtUs'>
-            <Row>
-                <Col sx={12}>
-                    <GoldSep />
-                </Col>
-            </Row>
-            <Row>
-                <Col sx={6} className='txt-container'>
-                    <h1>About our “Capricho”</h1>
-                    <p>
-                        Short explanation of the café’s name and the fact that “Capricho” 
-                        means whim in Spanish. Some background info as to who the small group 
-                        of entrepreneurs is, you know the basics. This page works well because 
-                        it’s for a start-up and cause this place has a background story in my 
-                        head that deserves to be told.
-                    </p>
-                    <div className='times'>Mon-Thu : 10h00 - 12h00 am</div>
-                    <div className='times'>Fri-Sun : 11h00 - 01h00 am</div>
-                    <div className='btn-container'>
-                        <GoldBtn href="#" prop='Check out the events'/>
-                        <GoldBtn href="#" prop='Check out the menu'/>
-                    </div>
-                </Col>
-                <Col sx={6} className='txt-container'>
-                    <h1>Placeholder</h1>
-                </Col>
-            </Row>
-
-
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <GoldSep />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={6} className='txt-container'>
+                        <h1>About our “Capricho”</h1>
+                        <p>
+                            Short explanation of the café’s name and the fact that “Capricho” 
+                            means whim in Spanish. Some background info as to who the small group 
+                            of entrepreneurs is, you know the basics. This page works well because 
+                            it’s for a start-up and cause this place has a background story in my 
+                            head that deserves to be told.
+                        </p>
+                        <div className='times'>Mon-Thu : 10h00 - 12h00 am</div>
+                        <div className='times'>Fri-Sun : 11h00 - 01h00 am</div>
+                        <div className='btn-container'>
+                            <GoldBtn href="#" prop='Check out the events'/>
+                            <GoldBtn href="#" prop='Check out the menu'/>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} className='map'>
+                        <h1>Location Map</h1>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
 
-export default AbtUs
+export default AbtUs;
