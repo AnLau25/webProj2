8194a5ab18353cfc838fefec4426434247b3a92b
AnLau25 <avald024@uottawa.ca>
2024-07-04 00:23:29 -0400

    w

--- public/Events.jsx
+++ public/Events.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Events.css';
 import './Bluebg.css';
 import GoldBtn from './GoldBtn';
-import { Row, Col, Container, Tabs, Tab } from 'react-bootstrap';
+import { Row, Col, Container, Tabs, Tab, Accordion } from 'react-bootstrap';
 
 const Events = () => {
     return (
@@ -17,13 +17,44 @@ const Events = () => {
                                         <div className='txt'>
                                             <h1>Library Collection</h1>
                                             <p>Short description of the way the library system works. A lil talk about the classics maybe talk abt the booking system.</p>
-                                            <GoldBtn href="#" prop='Browse our Collection'/>
+                                            <GoldBtn href="#" prop='Browse our Collection' />
                                         </div>
                                         <div className='txt'>
                                             <h1>Café and Menu</h1>
                                             <p>Specs about the menu, the time at which each menu happens, maybe talk about the chef.</p>
-                                            <GoldBtn href="#" prop='Check out the menu'/>
+                                            <GoldBtn href="#" prop='Check out the menu' />
                                         </div>
+                                    </Col>
+                                    <Col xs={12} md={6} className='bCard'>
+                                        <h1>QUESTIONS & RÉPONSES</h1>
+                                        <Container className='Acord-container'>
+                                            <Accordion defaultActiveKey="0" flush>
+                                                <Accordion.Item eventKey="0">
+                                                    <Accordion.Header>Mettre une première question ici ?</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        La réponse irait ici, l'engrenage fonctionne comme un toggle.
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                                <Accordion.Item eventKey="1">
+                                                    <Accordion.Header>Mettre une deuxième question ici ?</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        La réponse irait ici, l'engrenage fonctionne comme un toggle.
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                                <Accordion.Item eventKey="2">
+                                                    <Accordion.Header>Mettre une troisième question ici ?</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        La réponse irait ici, l'engrenage fonctionne comme un toggle.
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                                <Accordion.Item eventKey="3">
+                                                    <Accordion.Header>Mettre une quatrième question ici ?</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        La réponse irait ici, l'engrenage fonctionne comme un toggle.
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                            </Accordion>
+                                        </Container>
                                     </Col>
                                 </Row>
                             </div>
