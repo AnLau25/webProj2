f5548a3b9029b13100137892508979882d9a2ca4
AnLau25 <avald024@uottawa.ca>
2024-07-03 22:53:26 -0400

    u

--- public/Events.jsx
+++ public/Events.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Events.css';
 import './Bluebg.css';
+import GoldBtn from './GoldBtn';
 import { Row, Col, Container, Tabs, Tab } from 'react-bootstrap';
 
 const Events = () => {
@@ -11,7 +12,20 @@ const Events = () => {
                     <Tabs defaultActiveKey="rdrs" className="custom-tabs">
                         <Tab eventKey="rdrs" title="Readers">
                             <div className='tab-content'>
-                                <div className='placeholder'>Readers</div>
+                                <Row>
+                                    <Col xs={12} md={6} className='txt-container'>
+                                        <div className='txt'>
+                                            <h1>Library Collection</h1>
+                                            <p>Short description of the way the library system works. A lil talk about the classics maybe talk abt the booking system.</p>
+                                            <GoldBtn href="#" prop='Browse our Collection'/>
+                                        </div>
+                                        <div className='txt'>
+                                            <h1>Café and Menu</h1>
+                                            <p>Specs about the menu, the time at which each menu happens, maybe talk about the chef.</p>
+                                            <GoldBtn href="#" prop='Check out the menu'/>
+                                        </div>
+                                    </Col>
+                                </Row>
                             </div>
                         </Tab>
                         <Tab eventKey="wrtrs" title="Writers">
