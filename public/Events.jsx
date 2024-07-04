8cf94992e824b943e69251030480e58cdeae7f58
AnLau25 <avald024@uottawa.ca>
2024-07-04 15:41:02 -0400

    l

--- public/Events.jsx
+++ public/Events.jsx
@@ -13,6 +13,18 @@ const Events = () => {
                         <Tab eventKey="rdrs" title="Readers">
                             <div className='tab-content'>
                                 <Row>
+                                    <Col xs={12} md={6} className='txt-container'>
+                                        <div className='txt'>
+                                            <h1>Library Collection</h1>
+                                            <p>Short description of the way the library system works. A lil talk about the classics maybe talk abt the booking system.</p>
+                                            <GoldBtn href="#" prop='Browse our Collection' />
+                                        </div>
+                                        <div className='txt'>
+                                            <h1>Café and Menu</h1>
+                                            <p>Specs about the menu, the time at which each menu happens, maybe talk about the chef.</p>
+                                            <GoldBtn href="#" prop='Check out the menu' />
+                                        </div>
+                                    </Col>
                                     <Col xs={12} md={6} className='events-card'>
                                         <h1>Events of the week</h1>
                                         <Container className='accord-container'>
@@ -46,18 +58,6 @@ const Events = () => {
                                                 </Accordion.Item>
                                             </Accordion>
                                         </Container>
-                                    </Col>
-                                    <Col xs={12} md={6} className='txt-container'>
-                                        <div className='txt'>
-                                            <h1>Library Collection</h1>
-                                            <p>Short description of the way the library system works. A lil talk about the classics maybe talk abt the booking system.</p>
-                                            <GoldBtn href="#" prop='Browse our Collection' />
-                                        </div>
-                                        <div className='txt'>
-                                            <h1>Café and Menu</h1>
-                                            <p>Specs about the menu, the time at which each menu happens, maybe talk about the chef.</p>
-                                            <GoldBtn href="#" prop='Check out the menu' />
-                                        </div>
                                     </Col>
                                 </Row>
                             </div>
