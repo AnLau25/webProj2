d69c5beae18a1a1519e7071c887791e40a7be953
AnLau25 <avald024@uottawa.ca>
2024-07-07 17:12:44 -0400

    o

--- public/GoldCard.jsx
+++ public/GoldCard.jsx
@@ -4,28 +4,27 @@ import './GoldCard.css';
 const GoldCard = () => {
     return (
         <div className='gold-card'>
-            <h1 className='gold-card__header'><em>CONTACTEZ-NOUS</em></h1>
+            <h1 className='gold-card__header'>CONTACTEZ-NOUS</h1>
             <div className='gold-card__contact'>
                 <ul className='gold-card__contact-list'>
                     <li className='gold-card__contact-list-item bold'>
-                        <img src='wPhone.png' alt='Phone' />613 - 214 - 1031
+                        <img src='GTel.png' alt='Phone' />613 - 214 - 1031
                     </li>
                     <li className='gold-card__contact-list-item bold'>
-                        <img src='wMSG.png' alt='Email' />Insertlogo@here.com
+                        <img src='GMail.png' alt='Email' />Insertlogo@here.com
                     </li>
                     <li className='gold-card__contact-list-item gold-card__address'>
-                        <img src='wLoc.png' alt='Location' />
+                        <img src='GLoc.png' alt='Location' />
                         <div>
                             <div>33 Rue Lamagie, K1K 3F3</div>
-                            <div>55 Operator Priv, K1K 5C5</div>
                         </div>
                     </li>
                 </ul>
                 <div className='gold-card__socials'>
-                    <a href='#'><img src='bLink.png' alt='LinkedIn' /></a>
-                    <a href='#'><img src='bFace.png' alt='Facebook' /></a>
-                    <a href='#'><img src='bTweet.png' alt='Twitter' /></a>
-                    <a href='#'><img src='bInsta.png' alt='Instagram' /></a>
+                    <a href='#'><img src='GInsta.svg' alt='Instagram' /></a>
+                    <a href='#'><img src='GSnap.svg' alt='Snapchat' /></a>
+                    <a href='#'><img src='GTweet.svg' alt='Twitter' /></a>
+                    <a href='#'><img src='GPin.svg' alt='Pinterest' /></a>
                 </div>
             </div>
         </div>
