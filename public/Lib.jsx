7597db74e3cad7e77189466d44709aa008c0f177
AnLau25 <avald024@uottawa.ca>
2024-07-19 23:01:16 -0400

    s

--- public/Lib.jsx
+++ public/Lib.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Container } from 'react-bootstrap';
 import './Lib.css';
 import GoldSep from './GoldSep';
 import Foot from './Foot';
-
+import BCard from './bCard';
 
 const Lib = () => {
     return (
@@ -13,7 +13,7 @@ const Lib = () => {
                     <Row>
                         <Col>
                             <GoldSep />
-                           
+                            <BCard imgprop='./Tigri_1900.jpg' titleprop='Sandokan' langprop='It' sumprop='Il pirata Sandokan viene ferito durante una battaglia contro gli inglesi. Aiutato da Lord Guillonk, che lo cura in casa sua, conosce la sua bellissima nipote Marianna. Tra i due nasce un amore che sembra impossibile. Ma niente è impossibile per Sandokan!' />
                         </Col>
                     </Row>
                 </Container>
