c07e3f6e2b8cadf7dc85daf9798105bc24dbcb86
AnLau25 <avald024@uottawa.ca>
2024-12-13 10:31:53 -0500

    test col

--- public/Foot.jsx
+++ public/Foot.jsx
@@ -18,23 +18,25 @@ const Foot = () => {
           </Col>
         </Row>
         <Row>
-          <Col className="footer-content">
-            <Col xs={12} md={6}>
-              <div className="left-content">
-                <Link to="/#home" className="logo" onClick={() => handleNavLinkClick('home')}>
-                  <img src="./Tuntun_Logo1.svg" alt="Logo" className="navbar-brand" />
-                </Link>
-                <p>Copyright© Capricho de medianoche by Lupin powered by UO3525</p>
-              </div>
-            </Col>
-            <Col xs={12} md={6}>
-              <div align="end" className="right-content" >
-                <a href="#" className="social-icon"><img src="GInsta.svg" alt="Instagram" /></a>
-                <a href="#" className="social-icon"><img src="GSnap.svg" alt="Snapchat" /></a>
-                <a href="#" className="social-icon"><img src="GTweet.svg" alt="Twitter" /></a>
-                <a href="#" className="social-icon"><img src="GPin.svg" alt="Pinterest" /></a>
-              </div>
-            </Col>
+          <Col xs={12}>
+            <div className="footer-content">
+              <Col xs={12} md={6}>
+                <div className="left-content">
+                  <Link to="/#home" className="logo" onClick={() => handleNavLinkClick('home')}>
+                    <img src="./Tuntun_Logo1.svg" alt="Logo" className="navbar-brand" />
+                  </Link>
+                  <p>Copyright© Capricho de medianoche by Lupin powered by UO3525</p>
+                </div>
+              </Col>
+              <Col xs={12} md={6}>
+                <div align="end" className="right-content" >
+                  <a href="#" className="social-icon"><img src="GInsta.svg" alt="Instagram" /></a>
+                  <a href="#" className="social-icon"><img src="GSnap.svg" alt="Snapchat" /></a>
+                  <a href="#" className="social-icon"><img src="GTweet.svg" alt="Twitter" /></a>
+                  <a href="#" className="social-icon"><img src="GPin.svg" alt="Pinterest" /></a>
+                </div>
+              </Col>
+            </div>
           </Col>
         </Row>
       </Container>
